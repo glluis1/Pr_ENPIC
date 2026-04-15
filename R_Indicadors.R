af1c62d2aa282b4f04891b43a56552150b978d6a
glluis1 <glluis1@gmail.com>
2026-04-15 18:28:53 +0200

    Indicador 2

--- R_Indicadors.R
+++ R_Indicadors.R
@@ -37,3 +37,23 @@ round(IC_1$conf.int * 100, 2)
 
 # No tiene sentido el contraste de hipótesis porque al comparar 
 # con el estándar (100%, objetivo ideal) siempre dará significación
+
+
+# ---- Indicador 2 ----
+
+# Filtrado de pacientes con riesgo nutricional (NUTRIC Score > 5)
+datos_riesgo_nutr <- datos_uci48 %>% filter(NUTRIC_Score > 5)
+
+# Cálculo del porcentaje
+indic_2 <- datos_riesgo_nutr %>%
+  summarise(
+    n = n(),
+    x = sum(!is.na(VGSING2)),
+    porcentaje = (x / n) * 100
+  )
+
+# Intervalo de confianza
+IC_2 <- prop.test(indic_2$x, indic_2$n)
+round(IC_2$conf.int * 100, 2)
+
+# !! Pacientes sin NUTRIC Score no entran en el cálculo
