40231c3fa1e70d11d1f51efce11a1ead72414256
glluis1 <glluis1@gmail.com>
2026-04-21 11:07:56 +0200

    Indicador 8

--- R_Indicadors.R
+++ R_Indicadors.R
@@ -5,6 +5,7 @@ if (!require("haven")) {
 
 library(haven)
 library(dplyr)
+library(lubridate)
 
 
 # Carga de datos
@@ -62,10 +63,10 @@ round(IC_2$conf.int * 100, 2)
 # ---- Indicador 4a ----
 
 # Media del aporte calórico a partir del 4º día
-datos_calorias <- datos_uci48 %>%
+datos_aporte <- datos_uci48 %>%
   mutate(
     # 1. Cálculo de la media absoluta en kcal/día
-    media = rowMeans(pick(num_range("CALORIA", 4:14)), na.rm = TRUE),
+    media_calorias = rowMeans(pick(num_range("CALORIA", 4:14)), na.rm = TRUE),
     
     # 2. Cálculo del peso ideal
     peso_ideal = 25 * (TALLA^2),
@@ -76,14 +77,14 @@ datos_calorias <- datos_uci48 %>%
                            (PESOACT - peso_ideal) * 0.33 + peso_ideal),
     
     # 4. Cálculo de la media en kcal/kg/día
-    media_kg = media / peso_calculo
+    media_kg_calorias = media_calorias / peso_calculo
   )
 
 # Cálculo del porcentaje
-indic_4a <- datos_calorias %>%
+indic_4a <- datos_aporte %>%
   summarise(
     n = n(),
-    x = sum(media_kg >= 25 & media_kg <= 30, na.rm = TRUE),
+    x = sum(media_kg_calorias >= 25 & media_kg_calorias <= 30, na.rm = TRUE),
     porcentaje = (x / n) * 100
   )
 
@@ -99,22 +100,22 @@ IC_4a$p.value
 # Referència: Singer et al. (2019)
 
 # Días dentro del rango objetivo
-datos_calorias <- datos_calorias %>% 
+datos_aporte <- datos_aporte %>% 
   rowwise %>%
   mutate(
     # Días con datos (no NA)
-    n_dias = sum(!is.na(c_across(CALORIA4:CALORIA14))),
+    n_dias_cal = sum(!is.na(c_across(CALORIA4:CALORIA14))),
     
     # Días dentro del rango
-    n_dias_rango = sum(
+    n_dias_rango_cal = sum(
       (c_across(CALORIA4:CALORIA14) / peso_calculo) >= 25 &
         (c_across(CALORIA4:CALORIA14) / peso_calculo) <= 30,
       na.rm = TRUE
     ),
     
     # Porcentaje de cumplimiento
-    cumplimiento = if_else(n_dias > 0,
-                           (n_dias_rango / n_dias) * 100,
+    cumplimiento_cal = if_else(n_dias_cal > 0,
+                           (n_dias_rango_cal / n_dias_cal) * 100,
                            NA_real_)
   ) %>%
   ungroup()
@@ -123,28 +124,20 @@ datos_calorias <- datos_calorias %>%
 # ---- Indicador 4b ----
 
 # Media del aporte proteico a partir del 4º día
-datos_proteinas <- datos_uci48 %>%
+datos_aporte <- datos_aporte %>%
   mutate(
     # 1. Cálculo de la media absoluta en g/día
-    media = rowMeans(pick(num_range("PROT", 4:14)), na.rm = TRUE),
+    media_proteinas = rowMeans(pick(num_range("PROT", 4:14)), na.rm = TRUE),
     
-    # 2. Cálculo del peso ideal
-    peso_ideal = 25 * (TALLA^2),
-    
-    # 3. Peso a utilizar según IMC
-    peso_calculo = if_else(BMI < 30,
-                           PESOACT,
-                           (PESOACT - peso_ideal) * 0.33 + peso_ideal),
-    
-    # 4. Cálculo de la media en g/kg/día
-    media_kg = media / peso_calculo
+    # 2. Cálculo de la media en g/kg/día
+    media_kg_proteinas = media_proteinas / peso_calculo
   )
 
 # Cálculo del porcentaje
-indic_4b <- datos_proteinas %>%
+indic_4b <- datos_aporte %>%
   summarise(
     n = n(),
-    x = sum(media_kg >= 1.3, na.rm = TRUE),
+    x = sum(media_kg_proteinas >= 1.3, na.rm = TRUE),
     porcentaje = (x / n) * 100
   )
 
@@ -156,24 +149,41 @@ round(IC_4b$conf.int * 100, 2)
 IC_4b$p.value
 
 # Días dentro del rango objetivo
-datos_proteinas <- datos_proteinas %>% 
+datos_aporte <- datos_aporte %>% 
   rowwise %>%
   mutate(
     # Días con datos (no NA)
-    n_dias = sum(!is.na(c_across(PROT4:PROT14))),
+    n_dias_prot = sum(!is.na(c_across(PROT4:PROT14))),
     
     # Días dentro del rango
-    n_dias_rango = sum(
+    n_dias_rango_prot = sum(
       (c_across(PROT4:PROT14) / peso_calculo) >= 1.3,
       na.rm = TRUE
     ),
     
     # Porcentaje de cumplimiento
-    cumplimiento = if_else(n_dias > 0,
-                           (n_dias_rango / n_dias) * 100,
+    cumplimiento_prot = if_else(n_dias_prot > 0,
+                           (n_dias_rango_prot / n_dias_prot) * 100,
                            NA_real_)
   ) %>%
   ungroup()
+
+# ---- Indicador 4 ----
+
+# Cálculo del porcentaje conjunto
+indic_4 <- datos_aporte %>%
+  summarise(
+    n = n(),
+    x = sum(media_kg_calorias >= 25 & media_kg_proteinas >= 1.3, na.rm = TRUE),
+    porcentaje = (x / n) * 100
+  )
+
+# Intervalo de confianza
+IC_4 <- prop.test(indic_4$x, indic_4$n, p = 0.8, alternative = "less")
+round(IC_4$conf.int * 100, 2)
+
+# Valor p
+IC_4$p.value
 
 
 # ---- Indicador 6 ----
@@ -211,5 +221,43 @@ indic_7 <- datos_NPT %>%
 IC_7 <- prop.test(indic_7$x, indic_7$n, p = 0.9, alternative = "less")
 round(IC_7$conf.int * 100, 2)
 
+# Valor p
+IC_7$p.value
+
 
 # ---- Indicador 8 ----
+
+# Filtrado
+datos_NPC <- datos_aporte %>%
+  # 1. Pacientes con NE o NE-NPT y valores registradps del 4º día 
+  filter(TIPO_SN_Grupo %in% c(1, 3), !is.na(CALORIA4), !is.na(PROT4)) %>%
+  
+  # 2. Pacientes con <60% del aporte nutricional
+  filter(CALORIA4 < (15 * peso_calculo) | PROT4 < (0.8 * peso_calculo)) %>%
+  
+  # 3. Tiempo hasta inicio de NPC
+  mutate(
+    Inicio_NE = ymd_hms(Inicio_NE),
+    Inicio_NPT = ymd_hms(Inicio_NPT),
+    dias_npc = as.numeric(difftime(Inicio_NPT, Inicio_NE, units = "days"))
+  )
+  
+# Cálculo del porcentaje
+indic_8 <- datos_NPC %>%
+  summarise(
+    n = n(),
+    x = sum(dias_npc <= 5, na.rm = TRUE),
+    porcentaje = (x / n) * 100
+  )
+
+# Intervalo de confianza
+IC_8 <- prop.test(indic_8$x, indic_8$n, p = 0.9, alternative = "less")
+round(IC_8$conf.int * 100, 2)
+
+# Valor p
+IC_8$p.value
+
+# Aporte nutricional 60%: 15 kcal/kg/día y 0.8 g/kg/día
+
+
+# ---- Indicador 9 ----
